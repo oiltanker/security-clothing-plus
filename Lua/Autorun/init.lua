11945ababab0360ce73fecedde0ba5e15c8753be
Oiltanker <oiltankerdk@gmail.com>
2022-05-10 22:24:44 +0300

    fix for script not being run

--- Lua/Autorun/init.lua
+++ Lua/Autorun/init.lua
@@ -3,4 +3,6 @@ SecurityClothingPlus.Path = table.pack(...)[1]
 
 if SERVER then
     -- no server code
+elseif CLIENT then
+    dofile(SecurityClothingPlus.Path .. '/Lua/clothing_gun_holster.lua')
 end
